17d4342a139fb2c6e2355967c0dca5e105330c25
Nicholas Henry <nicholas.henry@gmail.com>
2015-04-10 14:05:52 -0400

    Move classes to independent files

--- lib/rblogger/platforms/blogger.rb
+++ lib/rblogger/platforms/blogger.rb
@@ -6,54 +6,8 @@ require 'extensions/google/api_client/installed_app_flow'
 module RBlogger
   class Blogger
     require 'rblogger/platforms/blogger/blog'
-
-    class Configuration
-      attr_reader :api_version, :cached_api_file, :credential_stored_file, :client_secrets_file
-
-      def initialize
-        @api_version = "v3"
-        @root_path = "#{Dir.home}/.rblogger/"
-        @credential_stored_file = "#{@root_path}/oauth2.json"
-        @cached_api_file = "#{@root_path}/api_#{@api_version}.cache"
-        @client_secrets_file = "#{@root_path}/client_secrets.json"
-      end
-    end
-
-    class Document
-      def initialize(client, configuration)
-        @client = client
-        @configuration = configuration
-      end
-
-      attr_reader :client, :configuration
-
-      # Load cached discovered API, if it exists. This prevents retrieving the
-      # discovery document on every run, saving a round-trip to API servers.
-      #
-      def fetch
-        if File.exists?(configuration.cached_api_file)
-          load_document
-        else
-          client.discovered_api('blogger', configuration.api_version).tap do |document|
-            save_document(document)
-          end
-        end
-      end
-
-      private
-
-      def load_document
-        File.open(configuration.cached_api_file) do |file|
-          Marshal.load(file)
-        end
-      end
-
-      def save_document(document)
-        File.open(configuration.cached_api_file, 'w') do |file|
-          Marshal.dump(document, file)
-        end
-      end
-    end
+    require 'rblogger/platforms/blogger/configuration'
+    require 'rblogger/platforms/blogger/document'
 
     def initialize
       @client = Google::APIClient.new(
---
+++ lib/rblogger/platforms/blogger/document.rb
@@ -0,0 +1,37 @@
+module RBlogger
+  class Blogger::Document
+    def initialize(client, configuration)
+      @client = client
+      @configuration = configuration
+    end
+
+    attr_reader :client, :configuration
+
+    # Load cached discovered API, if it exists. This prevents retrieving the
+    # discovery document on every run, saving a round-trip to API servers.
+    #
+    def fetch
+      if File.exists?(configuration.cached_api_file)
+        load_document
+      else
+        client.discovered_api('blogger', configuration.api_version).tap do |document|
+          save_document(document)
+        end
+      end
+    end
+
+    private
+
+    def load_document
+      File.open(configuration.cached_api_file) do |file|
+        Marshal.load(file)
+      end
+    end
+
+    def save_document(document)
+      File.open(configuration.cached_api_file, 'w') do |file|
+        Marshal.dump(document, file)
+      end
+    end
+  end
+end
