3e2d663fb9a901266164922652b4454427c4bb5b
Nicholas Henry <nicholas.henry@gmail.com>
2015-04-10 14:26:44 -0400

    Refactor internals of Blogger::Authorizer for readability

--- lib/rblogger/platforms/blogger/authorizer.rb
+++ lib/rblogger/platforms/blogger/authorizer.rb
@@ -7,21 +7,33 @@ module RBlogger
     attr_reader :configuration
 
     def authorize!
-      file_storage = Google::APIClient::FileStorage.new(configuration.credential_stored_file)
       if file_storage.authorization.nil?
-        client_secrets = Google::APIClient::ClientSecrets.load(configuration.client_secrets_file)
-        # The InstalledAppFlow is a helper class to handle the OAuth 2.0 installed
-        # application flow, which ties in with FileStorage to store credentials
-        # between runs.
-        flow = Google::APIClient::InstalledAppFlow.new(
-          :client_id => client_secrets.client_id,
-          :client_secret => client_secrets.client_secret,
-          :scope => 'https://www.googleapis.com/auth/blogger'
-        )
-        flow.authorize_cli(file_storage)
+        oauth_flow.authorize_cli(file_storage)
       else
         file_storage.authorization
       end
+    end
+
+    private
+
+    def file_storage
+      Google::APIClient::FileStorage.new(configuration.credential_stored_file)
+    end
+
+    # The InstalledAppFlow is a helper class to handle the OAuth 2.0 installed
+    # application flow, which ties in with FileStorage to store credentials
+    # between runs.
+    #
+    def oauth_flow
+      Google::APIClient::InstalledAppFlow.new(
+        :client_id => client_secrets.client_id,
+        :client_secret => client_secrets.client_secret,
+        :scope => 'https://www.googleapis.com/auth/blogger'
+      )
+    end
+
+    def client_secrets
+      @client_secrets ||= Google::APIClient::ClientSecrets.load(configuration.client_secrets_file)
     end
   end
 end
